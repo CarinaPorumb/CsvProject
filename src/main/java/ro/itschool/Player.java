8854871a983e0ff161a68a7be9f4f5e4a686096e
Carina <carina_porumb@yahoo.com>
2022-09-22 16:39:47 +0300

    Update

---
+++ src/main/java/ro/itschool/Player.java
@@ -0,0 +1,30 @@
+package ro.itschool;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Player {
+
+    //@CsvBindByName(column = "id")
+    @CsvBindByPosition(position = 0)
+    private int id;
+
+    @CsvBindByPosition(position = 1)
+    private String firstName;
+
+    @CsvBindByPosition(position = 2)
+    private String lastName;
+
+    @CsvBindByPosition(position = 3)
+    private int age;
+
+    @CsvBindByPosition(position = 4)
+    private int gsTitles;
+
+}
